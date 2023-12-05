42c895b65dc22cb686bcff7e62c0b6cf523156ac
FaridaBey <112944332+FaridaBey@users.noreply.github.com>
2023-12-05 20:52:36 +0200

    added the control unit

--- CU.v
+++ CU.v
@@ -1,26 +1,37 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 12/04/2023 04:11:39 PM
-// Design Name: 
 // Module Name: CU
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
 // Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module CU(
 
-    );
+module CU(input clk, input rst,input en ,input BTNL, input BTNR, output sel_1,output sel_2,
+input BTNC,z_flag, b0, output done, rst, load, enable, Psel);
+
+
+  reg updown;
+ 
+  always @ (posedge clk) begin
+   if(BTNL || BTNR) begin
+    if (BTNL) begin
+      updown <= 1'b1;
+    end
+    else if (BTNR) begin
+      updown <= 1'b0;
+    end
+  end
+  if(BTNC)begin
+   load <= 1'b1;
+   enable<= 1'b1;
+   end
+   if(z_flag)begin
+   done <=  1'b1;
+   //enable bcd??
+   end
+  if(b0)begin
+  Psel <=  1'b1;
+  end
+  end
+updown_counter #(2, 3) counter_inst(.clk(clk),.reset(rst), .enable(en), .UpDown(updown),.count({sel_2,sel_1}));
 endmodule
